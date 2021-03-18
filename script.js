0600c092f1365d7ac9d99812903b5ee681b9ecde
deg <deglucero@gmail.com>
2021-03-17 21:38:07 -0400

    add the bos for main page

---
+++ script.js
@@ -0,0 +1,14 @@
+function createDiv() {
+         var divTag = document.createElement("div");
+         divTag.id = "div1";
+         divTag.setAttribute("align", "center");
+         divTag.style.margin = "0px auto";
+         divTag.className = "dynamicDiv";
+         divTag.innerHTML = "This HTML Div tag is created dynamically using Javascript.";
+         document.body.appendChild(divTag);
+     }
+    
+
+     function createDiv() {
+         var divTag = document.createElement("div");
+     }
